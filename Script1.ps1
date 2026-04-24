31e58f3526115d86ccfc2b53dc33674ac81becfe
Jack-Pelham <jmpelham1025@gmail.com>
2026-04-24 12:00:21 -0500

    Fourth part of script done, will be updated further later

--- Script1.ps1
+++ Script1.ps1
@@ -16,6 +16,36 @@ $freeDisk = $disk.FreeSpace
 $usedDisk = $totalDisk - $freeDisk
 $diskUsage = [math]::Round(($usedDisk / $totalDisk) * 100, 2)
 
-Write-Host "CPU Usage: $cpuUsage%"
-Write-Host "Memory Usage: $memUsage%"
-Write-Host "Disk Usage (C: drive): $diskUsage%"
+#couldn't do write-host as it only shows in console, not report, so I wrapped the values
+
+#create structured object from calculated values
+$systemStats = [PSCustomObject]@{
+	CPU_Usage_Percent = "$cpuUsage%"
+	Memory_Usage_Percent = "$memUsage%"
+	Disk_Usage_Percent = "$diskUsage%"
+
+#Start doing 2nd part of script here
+
+#This is the start of the fourth part
+}
+#CSS for better view
+$style = @"
+<style>
+body { font-family: Arial; }
+h1 { color: #2E8C1; }
+table { border-collapse: collapse; width: 50%; }
+th, td { border: 1px solid black; padding: 8px; text-allign: left; }
+th { background-color: #f2f2f2; } 
+</style>
+"@
+
+#HTML Body
+$body = @"
+<h1>PC Health Report</h1>
+<p>Generated: $(Get-Date)</p>
+
+<h2>System Usage Summary</h2>
+$($systemStats | ConvertTo-Html -Fragment)
+"@
+
+ConvertTo-Html -Head $style -Body $body | Out-File "C:\Users\jackm\HWX\HealthReport.html"
